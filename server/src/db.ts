b8032451fda171192ea06cc10fdfca468b90fa11
Vladislav Yakovlev <normsvlad@yandex.ru>
2023-03-03 23:53:58 +0500

    web fix

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,15 @@
 import { Sequelize } from 'sequelize';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
-  password: '123456',
-  username: 'postgres',
-  database: 'phone_book',
-  port: 5432,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   define: {
     timestamps: false,
   },
